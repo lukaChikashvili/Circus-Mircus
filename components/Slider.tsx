de9d6d89920ccddaf4203157933aac00c9047cf4
luka chikashvili <lukachikashvili2020@gmail.com>
2024-06-21 17:24:36 +0400

    add smooth transitions

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -6,30 +6,70 @@ import img2 from '../assets/image0_0 (2).jpg'
 import img3 from '../assets/image1_0 (1).jpg'
 import img4 from '../assets/image1_0 (3).jpg'
 import Image from 'next/image';
+import { useTransition, animated } from 'react-spring';
 
 const images = [
    img1, img2, img3, img4
 ]
 
 
+
 const Slider = () => {
-   
+
+
     const [index, setIndex] = useState(0);
+
+        // react spring transitions
+const transitions = useTransition(index, {
+    key: index,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: 1000 },
+});
+
+   
 
     useEffect(() => {
       const interval = setInterval(() => {
         setIndex((prev) => (prev + 1) % images.length);
-      }, 5000);
+      }, 7000);
 
       return () => clearInterval(interval);
-    }, [])
+    }, []);
+
+  
 
   return (
     <div className='w-full duration-500 '>
      
-        <Image alt = "img" src = {images[index]} fill className='w-full h-screen object-cover ' />
-       
+     {transitions((style, i) => (
+        <animated.div
+          key={i}
+          style={{
+            ...style,
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+          }}
+        >
+          <Image
+            alt="img"
+            src={images[i]}
+            layout="fill"
+            objectFit="cover"
+            style={{ opacity: 0.8 }} 
+          />
+        </animated.div>
+      ))}
     </div>
+   
   )
 }
 
